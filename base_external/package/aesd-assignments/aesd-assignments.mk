8cf0694add51bbf1082cb2e49c28d2b4620b9f70
Daniel ferrans <151735067+diferrans1@users.noreply.github.com>
2025-09-12 14:47:33 -0400

    chore: aarch64 pending tag

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -13,9 +13,15 @@ AESD_ASSIGNMENTS_VERSION = '189c58023fc49bf1f32eb9dba5c84a8c0a2978fd'
 AESD_ASSIGNMENTS_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-diferrans1.git'
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
-
+# $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
 define AESD_ASSIGNMENTS_BUILD_CMDS
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
+	@echo "Building finder-app in $(@D)/finder-app for target $(TARGET_ARCH)"
+	$(MAKE) -C $(@D)/finder-app \
+	    CC="$(TARGET_CC)" \
+	    LD="$(TARGET_LD)" \
+	    CFLAGS="$(TARGET_CFLAGS)" \
+	    LDFLAGS="$(TARGET_LDFLAGS)" \
+	    all
 endef
 
 # TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
@@ -23,6 +29,10 @@ define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -d 0755 $(@D)/conf/ $(TARGET_DIR)/etc/finder-app/conf/
 	$(INSTALL) -m 0755 $(@D)/conf/* $(TARGET_DIR)/etc/finder-app/conf/
 	$(INSTALL) -m 0755 $(@D)/assignment-autotest/test/assignment4/* $(TARGET_DIR)/bin
+	# ADD writer
+	$(INSTALL) -m 0755 $(@D)/finder-app/writer $(TARGET_DIR)/bin
+	# Full finder-test.sh
+	$(INSTALL) -m 0755 $(@D)/finder-app/finder-test.sh $(TARGET_DIR)/bin
 endef
 
 $(eval $(generic-package))
