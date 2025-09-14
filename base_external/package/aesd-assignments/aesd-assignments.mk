27d92c5afc229776c3fa74e30554de05dcc20734
Daniel ferrans <151735067+diferrans1@users.noreply.github.com>
2025-09-14 10:30:21 -0400

    chore: update package version

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = '189c58023fc49bf1f32eb9dba5c84a8c0a2978fd'
+AESD_ASSIGNMENTS_VERSION = '94ffd476fc5f36c17f55f0f6c1ef2af14742cd9e'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
